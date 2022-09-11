dd60592b43db5e97eb1db62727ccbd3fbafb832a
Pearl Molefe <PMolefe@bec.co.bw>
2022-09-11 10:31:04 +0200

    adding aws logging

--- cb.api/Program.cs
+++ cb.api/Program.cs
@@ -23,10 +23,22 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri(baseUrl)
 });
 
+//Mysql from user secrets
+//builder.Configuration.AddEnvironmentVariables().AddUserSecrets<Program>();
+//var connectionString = builder.Configuration.GetConnectionString("mysqlconnection");
+
+//Get connection string from env
+builder.Configuration.AddEnvironmentVariables();
+var connectionString = Environment.GetEnvironmentVariable("RDS_DB_CONN_STRING");
+Console.WriteLine(connectionString);
+
 //AWS SETUP
 // Configures default options like AWS region - if you don't provide any, its taken from the AWS Fargate environment
 var awsOptions = builder.Configuration.GetAWSOptions();
 awsOptions.Credentials = new EnvironmentVariablesAWSCredentials();
+
+var cred = awsOptions.Credentials.GetCredentials();
+Console.WriteLine(cred);
 builder.Services.AddDefaultAWSOptions(awsOptions);
 
 // Makes the Amazon CloudWatch SDK available in the DI container
@@ -40,14 +52,7 @@ builder.Services.AddLogging(loggingBuilder =>
     loggingBuilder.SetMinimumLevel(LogLevel.Debug);
 });
 
-//Mysql from user secrets
-//builder.Configuration.AddEnvironmentVariables().AddUserSecrets<Program>();
-//var connectionString = builder.Configuration.GetConnectionString("mysqlconnection");
 
-//Get connection string from env
-builder.Configuration.AddEnvironmentVariables();
-var connectionString = Environment.GetEnvironmentVariable("RDS_DB_CONN_STRING");
-Console.WriteLine(connectionString);
 
 //db connection
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseMySQL(connectionString));
